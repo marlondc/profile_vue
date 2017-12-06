aa7cceb4b80c42cf2f5f25d5fa84b2a9aefbed32
marlondc <mdatechong@gmail.com>
2017-12-06 15:44:27 +0000

    change port

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+app.listen(8080, () => {
+  console.log('listening on port 8080');
 });
